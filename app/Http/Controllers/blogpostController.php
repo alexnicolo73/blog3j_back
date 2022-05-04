7b8f2ed3691703f7545083f72a919d6a702d554a
alexnicolo73 <alex.nicolo@le-campus-numerique.fr>
2022-05-04 15:33:25 +0200

    fix

--- app/Http/Controllers/blogpostController.php
+++ app/Http/Controllers/blogpostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class blogpostController extends Controller
+class BlogPostController extends Controller
 {
     //
 }
--- app/Models/blogpostModel.php
+++ app/Models/blogpostModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class blogpostModel extends Model
+class BlogPostModel extends Model
 {
     use HasFactory;
 }
